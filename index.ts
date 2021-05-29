3f70b37c8d7a4ab1141ca08bb883a83589988a52
Saad Ahmed <saad.sengr@gmail.com>
2021-05-29 19:27:31 +0500

    pkg json added.

--- index.ts
+++ index.ts
@@ -90,7 +90,7 @@ app.post('/faucet/:token/:address', async (req, res) => {
 
 
 // this is a simple route to make sure everything is working properly
-const runningMessage = `Server running at http://localhost:${process.env.PORT}`;
+const runningMessage = `🚰 Server running at http://localhost:${process.env.PORT}`;
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(runningMessage)
 });
